3926344338054fefccc128e6cb5e5e584a13f003
Gal Trapečar <trapecar.gal@gmail.com>
2022-12-31 10:41:33 +0100

    Initial Commit

---
+++ dexter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  dexter
+//
+//  Created by Dev on 31/12/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ dexter/dexterApp.swift
@@ -0,0 +1,17 @@
+//
+//  dexterApp.swift
+//  dexter
+//
+//  Created by Dev on 31/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct dexterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
